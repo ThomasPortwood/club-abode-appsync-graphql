560aa295b9b42845db67fd1484e06acc163c069a
Thomas Portwood <thomas.w.portwood@gmail.com>
2020-06-27 20:49:35 -0400

    demo schema

--- terraform/aws-appsync-graphql.tf
+++ terraform/aws-appsync-graphql.tf
@@ -5,9 +5,35 @@ resource "aws_appsync_graphql_api" "club-abode-appsync-graphql" {
   schema = <<EOF
 schema {
     query: Query
+    mutation: Mutation
+    subscription: Subscription
 }
+
+type Mutation {
+    addPost(id: ID! author: String! title: String content: String url: String): Post!
+    updatePost(id: ID! author: String! title: String content: String url: String ups: Int! downs: Int! expectedVersion: Int!): Post!
+    deletePost(id: ID!): Post!
+}
+
+type Post {
+    id: ID!
+    author: String!
+    title: String
+    content: String
+    url: String
+    ups: Int
+    downs: Int
+    version: Int!
+}
+
 type Query {
-  test: Int
+    allPost: [Post]
+    getPost(id: ID!): Post
+}
+
+type Subscription {
+    newPost: Post
+    @aws_subscribe(mutations:["addPost"])
 }
 EOF
 }
